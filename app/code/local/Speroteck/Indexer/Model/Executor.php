9d2935a08b02bf62919f66e7e77d779e9227c411
Volodymyr Dubyna <vdubyna@speroteck.com>
2013-11-05 12:42:14 +0200

    Add cronjob to reindex everything each night

---
+++ app/code/local/Speroteck/Indexer/Model/Executor.php
@@ -0,0 +1,35 @@
+<?php
+
+class Speroteck_Indexer_Model_Executor
+{
+
+    /**
+     * Reindex everything by cron
+     *
+     * @return array
+     */
+    public static function reindexEverything()
+    {
+        $processes = array();
+        /** @var Mage_Index_Model_Indexer $indexer */
+        $indexer = Mage::getSingleton('index/indexer');
+
+        $collection = $indexer->getProcessesCollection();
+        foreach ($collection as $process) {
+            $processes[] = $process;
+        }
+
+        foreach ($processes as $process) {
+            /* @var $process Mage_Index_Model_Process */
+            try {
+                $process->reindexEverything();
+            } catch (Mage_Core_Exception $e) {
+                Mage::logException($e);
+            } catch (Exception $e) {
+                Mage::logException($e);
+            }
+        }
+
+        return $processes;
+    }
+}
