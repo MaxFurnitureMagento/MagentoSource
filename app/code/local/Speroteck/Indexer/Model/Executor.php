b7bf7998000d8dee8c3e23fc8210f9399277bf25
Volodymyr Dubyna <vdubyna@speroteck.com>
2013-11-06 16:32:21 +0200

    Add messaging for cron

--- app/code/local/Speroteck/Indexer/Model/Executor.php
+++ app/code/local/Speroteck/Indexer/Model/Executor.php
@@ -22,17 +22,21 @@ class Speroteck_Indexer_Model_Executor
             }
         }
 
+        $result = array("Reindex only required indexers");
         foreach ($processes as $process) {
             /* @var $process Mage_Index_Model_Process */
             try {
                 $process->reindexEverything();
+                $result[$process->getIndexerCode()] = 'Successfully finished';
             } catch (Mage_Core_Exception $e) {
                 Mage::logException($e);
+                $result[$process->getIndexerCode()] = "ERROR: " . $e->getMessage();
             } catch (Exception $e) {
                 Mage::logException($e);
+                $result[$process->getIndexerCode()] = "ERROR: " . $e->getMessage();
             }
         }
 
-        return $processes;
+        return print_r($result, 1);
     }
 }
